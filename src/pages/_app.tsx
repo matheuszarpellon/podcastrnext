45c1b8dfb2fd67b09a3a97acb023da34406c51a1
matheuszarpellon <mvzarpellon@gmail.com>
2021-04-24 10:32:42 -0300

    Checkpoint aula 4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,33 @@ import '../styles/global.scss'
 
 import { Header } from '../components/Header'
 import { Player } from '../components/Player'
+import { PlayerContext } from '../contexts/PlayerContext'
+import { useState } from 'react'
 
 import styles from '../styles/app.module.scss'
 
+
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true)
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
   return (
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
     <div className={styles.wrapper}>
       <main>
       <Header />
@@ -14,6 +36,7 @@ function MyApp({ Component, pageProps }) {
       </main>
       <Player />
     </div>
+    </PlayerContext.Provider>
   )
 }
 
